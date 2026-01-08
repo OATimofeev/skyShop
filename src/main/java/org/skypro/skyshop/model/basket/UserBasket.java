72cd1375fe1168d126123317f1a7e20b950742ad
Oleg Timofeev <oleg.timofeev@rtmis.ru>
2026-01-08 14:01:04 +0300

    ДЗ: Жизненный цикл компонентов Spring Boot приложения

---
+++ src/main/java/org/skypro/skyshop/model/basket/UserBasket.java
@@ -0,0 +1,20 @@
+package org.skypro.skyshop.model.basket;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class UserBasket {
+
+    private final List<BasketItem> itemList;
+    private final Integer total;
+
+    public UserBasket(List<BasketItem> itemList) {
+        this.itemList = itemList;
+        total = itemList
+                .stream()
+                .mapToInt(item -> item.product().getPrice() * item.count())
+                .sum();
+    }
+}
